4c972041d7c06d517912a0760a8fe103715e0024
MarcusE-creator <70136517+MarcusE-creator@users.noreply.github.com>
2021-04-28 09:14:47 +0200

    image pasting

--- run.py
+++ run.py
@@ -2,13 +2,24 @@ from text_segmentation import img_segmentation
 from image_paste import image_paste
 import cv2 as cv
 
-img_path = 'input3.tif'
-cropped_images = img_segmentation(img_path)
-pasted_images = image_paste(cropped_images)
 
-cv.imshow('Origin', cv.imread(img_path))
-cv.waitKey(0)
-
-for image in pasted_images:
-    cv.imshow('result', image)
+def get_small_images(img_path):
+    cv.imshow('Origin', cv.imread(img_path))
     cv.waitKey(0)
+
+    cropped_images = img_segmentation(img_path)
+    pasted_images = image_paste(cropped_images)
+
+    return pasted_images
+
+
+def show_images(images):
+    for image in images:
+        image = cv.resize(image, (100, 100))
+        cv.imshow('image', image)
+        cv.waitKey(0)
+
+
+path = 'input3.tif'
+small_images = get_small_images(path)
+show_images(small_images)
