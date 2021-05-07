50bc006afcf7bf0766229ebf139f3f0b9f98b9f9
MarcusE-creator <70136517+MarcusE-creator@users.noreply.github.com>
2021-05-07 14:09:45 +0200

    neural network model fixes

--- OCR.py
+++ OCR.py
@@ -1,18 +1,18 @@
 from text_segmentation import img_segmentation, image_paste
 import cv2 as cv
 import os
-from nn_model import train_neural, predict_single_img, load_img, sorted_alphanumeric
+from CNN_Model import train_cnn
 import pickle
 from tensorflow import keras
 
 
-def new_model(model_typ, file_name):
+def new_model(model_typ, file_name, seed, img_am, epochs):
     model = None
     if file_name in os.listdir("model"):
         os.remove(f"model/{file_name}")
     if model_typ.lower() == "neural":
-        model = train_neural()
-        model.save('model/{file_name}')
+        model = train_cnn(seed, img_am, epochs)
+        model.save(f'model/{file_name}')
     if model_typ.lower() == "forest":
         model = train_forest()
         with open(f"model/{file_name}", "wb") as file:
@@ -41,29 +41,13 @@ def random_forest_ocr():
 
 def neural_network_ocr():
     # New model
-    # model = new_model('neural', 'nn_model')
+    seed = 8
+    img_am = 1000
+    epochs = 20
+    new_model('neural', 'nn_model', seed, img_am, epochs)
 
     # Load model
     # model = keras.models.load_model('model/nn_model')
 
-    cropped_images = img_segmentation('input.tif')
 
-    # TODO Fix ugly reversed onehot encoding
-    char_dict = {"char": [], "matrix": []}
-    char_set = dict()
-    src = r"model/dataset/handwritten_letters"
-    print('Loading images...')
-    for i in range(1000):
-        for folder in os.listdir(src):
-            if folder not in char_set:
-                char_set[folder] = sorted_alphanumeric(os.listdir(src + '/' + folder))
-            char_dict["matrix"].append('-')
-            char_dict["char"].append(folder)
-
-    chars = char_dict['char'][:35]
-
-    for image in cropped_images:
-        print(predict_single_img(model, image.flatten(), chars))
-        image = cv.resize(image, (100, 100))
-        cv.imshow('image', image)
-        cv.waitKey(0)
+neural_network_ocr()
